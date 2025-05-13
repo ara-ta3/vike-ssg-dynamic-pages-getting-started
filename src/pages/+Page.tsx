2b8cd9df50afe1be92c33fee78f75eb3411515a5
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2025-05-13 21:07:19 +0900

    Fix

--- src/pages/+Page.tsx
+++ src/pages/+Page.tsx
@@ -7,8 +7,12 @@ function Page() {
   return (
     <div>
       <h1>Hello World</h1>
-      <a href="/hoge/1/">to hoge1</a>
-      <a href="/hoge/2/">to hoge2</a>
+      <p>
+        <a href="/hoge/1/">to hoge1</a>
+      </p>
+      <p>
+        <a href="/hoge/2/">to hoge2</a>
+      </p>
       <p>now: {c}</p>
       <button type="button" onClick={() => setC(c + 1)}>
         Count Up
